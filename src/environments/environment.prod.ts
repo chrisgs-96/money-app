e260010ccf459cc2338596e08a24aecf41f119b0
chrisgs-96 <chrisgs96@outlook.com>
2022-11-20 20:04:46 +0200

    prod build

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig: {
+    apiKey: "AIzaSyCXqTYXh9oxtsvBkNupeCdn8pYLcsVOf0U",
+    authDomain: "money-manager-764a8.firebaseapp.com",
+    databaseURL: "https://money-manager-764a8-default-rtdb.europe-west1.firebasedatabase.app",
+    projectId: "money-manager-764a8",
+    storageBucket: "money-manager-764a8.appspot.com",
+    messagingSenderId: "728138341088",
+    appId: "1:728138341088:web:81dcd6b958c84784f40e85"
+  }
 };
